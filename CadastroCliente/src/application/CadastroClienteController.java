ea96b981a268b772c744e50d0280d755aa11620f
Computador <Computador@CPU-Ale>
2021-11-21 16:12:56 -0300

    Tudo funcionando!!! Grava no BD.

--- CadastroCliente/src/application/CadastroClienteController.java
+++ CadastroCliente/src/application/CadastroClienteController.java
@@ -1,14 +1,13 @@
 package application;
 
-import java.time.Instant;
 import java.time.LocalDate;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import javax.swing.JOptionPane;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
@@ -17,91 +16,94 @@ public class CadastroClienteController {
 
     @FXML
     private Button btnCancelar;
-
     @FXML
     private Button btnGravar;
-
     @FXML
     private TextField textFieldCelular;
-
     @FXML
     private TextField textFieldCpf;
-
     @FXML
     private DatePicker textFieldDateNascimento;
-
     @FXML
     private TextField textFieldEmail;
-
     @FXML
     private TextField textFieldNome;
-
     @FXML
     void handlerCancelar(ActionEvent event) {
     	((Node)(event.getSource())).getScene().getWindow().hide();  // fecha janela
     }
     
-    LocalDate dataHoje = LocalDate.now(); 
-
-    Cliente cliente = new Cliente();
-    ClienteDao clienteDAO = new ClienteDao();
+    LocalDate dataHoje = LocalDate.now(); // Instancia objeto com a data de hoje
+    Cliente cliente = new Cliente();  // Instancia objeto cliente
+    ClienteDao clienteDAO = new ClienteDao(); // Instancia objeto clienteDao
     
     @FXML
     void handlerGravar(ActionEvent event) {
-    	// pode cliar um metodo para preecher o cliente  tipo: this.preencherCliente
-    	// 1-pega os valores dos textfields  // verifica se campos estão vazios, e campos obrigatórios
-    	// 2-grava os valores no objeto cliente
-    	// 3-grava o objeto cliente no BD
+    	Object[] options = { "SIM", "CANCELAR" };
+		int opcao = JOptionPane.showOptionDialog(null, "Confirma a gravação dos dados?", "CONFIRMAÇÃO", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
+    	if (opcao==0) {
+    		if (this.validarCliente()) {
+    			this.construirCliente();
+		    	try {
+					clienteDAO.inserir(cliente);  // gravar no BD
+					limparCampos(); //limpar campos textField
+					((Node)(event.getSource())).getScene().getWindow().hide();  // fecha janela
+					JOptionPane.showMessageDialog(null, "Cadastrado com Sucesso!!", "Sucesso", JOptionPane.INFORMATION_MESSAGE, null);
+				}catch(Exception ex){
+		            JOptionPane.showMessageDialog(null, "Verifique o ClienteDAO.", "Problemas com Banco de Dados.", JOptionPane.ERROR_MESSAGE);  
+		            throw ex;
+				}
+			}
+		} else {
+			System.exit(0);
+		}
+    }
+    private Boolean validarCliente() {
+    	StringBuilder listaErrosList = new StringBuilder();
+    	Boolean isValid = true;
+    	if (textFieldNome.getText().isEmpty() || textFieldCpf.getText().isEmpty() ) { // não pode ser campo vazio
+    		listaErrosList.append("O campo Nome e/ou o campo CPF não podem ser vazio.");
+    		listaErrosList.append("\n");
+    		isValid = false;
+		}
+    	if (!textFieldCpf.getText().matches("^[0-9]+$")) { // somente números
+    		listaErrosList.append("O campo CPF deve conter somente números.");
+    		listaErrosList.append("\n");
+    		isValid = false;
+    	}
+    	if (isValid == false) {
+	    	Alert alert = new Alert(AlertType.INFORMATION);
+	    	alert.setTitle("Dados Incorretos!");
+	    	alert.setHeaderText("Campos Inválidos:");
+	    	alert.setContentText(listaErrosList.toString());
+	    	alert.showAndWait();
+    	}
+    	return isValid;
+    }
+    private void construirCliente() {
     	try {
-    		
-    		// fase captura de dados
-	    	String campoNome = textFieldNome.getText();
-	    	String campoCPF = textFieldCpf.getText();
-	    	String campoCelular = textFieldCelular.getText();
-	    	// pega no sistema data atual que será a data do cadastro
+	    	// fase instanciar objeto
+	    	cliente.setNome(textFieldNome.getText());
+	    	cliente.setCpf(Long.valueOf(textFieldCpf.getText()));
+	    	cliente.setCelular(textFieldCelular.getText());
 	    	Date dataCadastro = java.sql.Date.valueOf(dataHoje);  // converte LocalDate para Date
+	    	cliente.setDatacadastro(dataCadastro);
 	    	LocalDate campoDataNascimento = textFieldDateNascimento.getValue();
 	    	Date dataNascimento = java.sql.Date.valueOf(campoDataNascimento);  // converte LocalDate para Date
-	    	String campoEMail = textFieldEmail.getText();
-    		
-	    	// fase instanciar objeto
-	    	cliente.setNome(campoNome);
-	    	cliente.setCpf(Long.valueOf(campoCPF));
-	    	cliente.setCelular(campoCelular);
-	    	DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy"); // define padrão das datas BR
-	    	cliente.setDatacadastro(dataCadastro);
-	    	cliente.setDatanascimento(dataNascimento);  // altera o atributo
-			cliente.setEmail(campoEMail);
+	    	cliente.setDatanascimento(dataNascimento);
+			cliente.setEmail(textFieldEmail.getText());
     	}catch(Exception ex){
     		JOptionPane.showMessageDialog(null, "Verifique a fase instanciar objeto.", "Problemas com o objeto", JOptionPane.ERROR_MESSAGE);  
             throw ex;
     	}
-		
-    	
-		try{
-			// fase gravar no BD
-			clienteDAO.inserir(cliente);  // gravar no BD
-			
-			//limpar campos textField
-			textFieldNome.setText("");
-			textFieldCpf.setText("");
-			textFieldCelular.setText("");
-			textFieldDateNascimento.setPromptText("");
-			textFieldEmail.setText("");
-			
-			
-			JOptionPane.showMessageDialog(null, "Cadastrado com Sucesso!!");
-		}catch(Exception ex){
-            JOptionPane.showMessageDialog(null, null, "Problemas com Banco de Dados.", JOptionPane.ERROR_MESSAGE);  
-            throw ex;
-		}
-		
-		
-    	
-    	((Node)(event.getSource())).getScene().getWindow().hide();  // fecha janela
     }
-    // cancelar --> fechar a tela; --- ok
-    // gravar --> salva os dados no banco e fecha a tela e dï¿½ mensagem de gravado com sucesso.
-    // tratar erros - campo obrigatórios
-    
+    private void limparCampos() {
+    	textFieldNome.clear();
+    	textFieldNome.clear();
+		textFieldCpf.clear();
+		textFieldCelular.clear();
+		textFieldDateNascimento.setValue(null);
+		textFieldEmail.clear();
+    }
+
 }
--- CadastroCliente/src/application/Main.java
+++ CadastroCliente/src/application/Main.java
@@ -1,21 +1,10 @@
 package application;
 	
-//import java.sql.Date;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
-import java.util.Calendar;
-import java.util.Locale;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-
-import java.util.Date;
 
 public class Main extends Application {
 	@Override
@@ -32,77 +21,7 @@ public class Main extends Application {
 			e.printStackTrace();
 		}
 	}
-	
-	
-    
-	
-	public static void main(String[] args) throws ParseException {
-		
-		/*
-		DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-		Date dataCadastro = new Date();
-		LocalDate dataNascimentoLocal = LocalDate.parse(dataCadastro, formato);  // aqui pega a data do fieldText
-		Date dataNascimentoDate = java.sql.Date.valueOf(dataNascimentoLocal);  // converte a data para tipo Date
-		System.out.println(dataNascimentoDate);
-		*/
-		
-		
+	public static void main(String[] args) {
 		launch(args);
-		/*
-		Cliente cliente = new Cliente();
-		cliente.setNome("Sebastiana das oreias");
-		System.out.println(cliente.getNome());
-		cliente.setCpf(Long.valueOf("524568262"));
-		System.out.println(cliente.getCpf());
-		cliente.setCelular("62 9983-5245");
-		System.out.println(cliente.getCelular());
-		
-		//SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
-		//System.out.println(formatter.format(calendar.getTime()));
-		//formatter.format(calendar.("23/11/2015"))
-		//pst.setDate(2, java.sql.Date.valueOf(ordem_Producao.getData_abertura()));
-		
-		//ps.setDate(2, java.sql.Date.valueOf("2013-09-04"));
-		
-		//DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy"); // define padrão das datas BR 
-		////Date outradata = java.sql.Date.valueOf(formato.format(dataCadastro)); // forma de usar o padrão
-		
-		//LocalDate dataCadastroLocal = LocalDate.parse("23/11/2015", formato);
-		//System.out.println(dataCadastroLocal);  // formato de data do sql
-		//java.util.Date dataCadastroDate = Date.from(dataCadastroLocal.atStartOfDay(ZoneId.systemDefault()).toInstant());
-		//System.out.println(dataCadastroDate);  // formato de data para mostrar na tela
-		// =================================== PEGAR DATA CADASTRO ==============================
-		
-		DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy"); // define padrão das datas BR
-		LocalDate dataCadastroLocal = LocalDate.parse("23/11/2015", formato);  // aqui pega a data do fieldText
-		Date dataCadastroDate = java.sql.Date.valueOf(dataCadastroLocal);  // converte a data para tipo Date
-		cliente.setDatacadastro(dataCadastroDate);  // altera o atributo
-		
-		// =================================== PEGAR DATA CADASTRO - FIM ==============================
-		LocalDate dataNascimentoLocal = LocalDate.parse("23/11/2015", formato);  // aqui pega a data do fieldText
-		Date dataNascimentoDate = java.sql.Date.valueOf(dataNascimentoLocal);  // converte a data para tipo Date
-		cliente.setDatanascimento(dataNascimentoDate);  // altera o atributo
-		// ===========================================================================================
-		cliente.setEmail("amanha@hoje.com");
-		ClienteDao clientedao = new ClienteDao();
-		clientedao.inserir(cliente);
-		
-		*/
-		
-		/*
-		 * LocalDate datetime = LocalDate.parse(txt_Data_Abertura.getText(), DateTimeFormatter.ofPattern(“dd/MM/yyyy”));
-String newstring = datetime.format(DateTimeFormatter.ofPattern(“dd/MM/yyyy”));
-System.out.println(newstring); 
-		 * 
-		 * 
-		 */
-		
 	}
 }
-
-
-
-	
-
-
-
